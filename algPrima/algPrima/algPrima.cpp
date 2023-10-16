0e2c251b885970f1e14499d44d09a9621a9cb78c
taofi <58530085+taofi@users.noreply.github.com>
2023-10-16 15:45:55 +0300

    new labs add

---
+++ algPrima/algPrima/algPrima.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <set>
+#define N 8
+
+using namespace std;
+
+int** prima(int mtrx[N][N])
+{
+    int n = 0;
+    int** newMtrx = new int* [N];
+    for (int i = 0; i < N; i++)
+    {
+        newMtrx[i] = new int[N];
+        for (int j = 0; j < N; j++)
+            newMtrx[i][j] = 0;
+    }
+    bool markedPoint[N];
+    int marked[N];
+    for (int i = 0; i < N; i++)
+        markedPoint[i] = false;
+
+    int minI, minJ, minCh = INT_MAX, lastMin;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (minCh > mtrx[i][j] && mtrx[i][j] != 0)
+            {
+                minI = i;
+                minJ = j;
+                minCh = mtrx[i][j];
+            }
+        }
+    }
+    marked[0] = minI;
+    marked[1] = minJ;
+    n += 2;
+    for (int i = 0; i < N; i++)
+    {
+        mtrx[minI][i] = 0;
+        mtrx[minJ][i] = 0;
+    }
+    newMtrx[minI][minJ] = minCh;
+
+    int m;
+    while (n < N)
+    {
+        minCh = INT_MAX;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (minCh > mtrx[i][marked[j]] && mtrx[i][marked[j]] != 0)
+                {
+                    minI = i;
+                    minJ = marked[j];
+                    minCh = mtrx[i][marked[j]];
+                }
+            }
+        }
+        if (markedPoint[minI])
+            m = minJ;
+        else
+            m = minI;
+        for (int i = 0; i < N; i++)
+            mtrx[m][i] = 0;
+        newMtrx[minI][minJ] = minCh;
+        markedPoint[m] = true;
+        marked[n] = m;
+        n++;
+    }
+
+    return newMtrx;
+}
+
+
+int main()
+{
+    int graph[N][N] =
+    {
+          {0, 2, 0,  8, 2, 0, 0, 0},
+          {2, 0, 3, 10, 5, 0, 0, 0},
+          {0, 3, 0, 0, 12, 0, 0, 7},
+          {8, 10, 0, 0, 14, 3, 1, 0},
+          {1, 5, 12, 14, 0, 11, 4, 8},
+          {0, 0, 0, 3, 11, 0, 6, 0},
+          {0, 0, 0, 1, 4, 6, 0, 9},
+          {0, 0, 7, 0, 8, 0, 9, 0},
+         
+    };
+    int cost = 0;
+    int** mtrx = prima(graph);
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cout << mtrx[i][j] << ' ';
+            if (mtrx[i][j] != 0)
+                cost += mtrx[i][j];
+        }
+        cout << endl;
+    }
+    cout << cost << endl;
+    for (int i = 0; i < N; ++i) {
+        delete[] mtrx[i]; 
+    }
+    delete[] mtrx;
+}
---
+++ kraskal/kraskal/kraskal.cpp
@@ -0,0 +1,88 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> parent;
+vector <int> rank1;
+
+void make_set(int v) {
+    parent[v] = v;
+    rank1[v] = 0;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (rank1[a] < rank1[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rank1[a] == rank1[b])
+            rank1[a]++;
+    }
+}
+
+struct Edge {
+    int u, v, weight;
+    bool operator<(Edge const& other) {
+        return weight < other.weight;
+    }
+    Edge(int U, int V, int W)
+    {
+        u = U;
+        v = V;
+        weight = W;
+    }
+};
+
+
+int main()
+{
+    int n = 8;
+    vector<Edge> edges;
+    int graph[8][8] =
+    {
+          {0, 2, 0,  8, 2, 0, 0, 0},
+          {0, 0, 3, 10, 5, 0, 0, 0},
+          {0, 0, 0, 0, 12, 0, 0, 7},
+          {0, 0, 0, 0, 14, 3, 1, 0},
+          {0, 0, 0, 0, 0, 11, 4, 8},
+          {0, 0, 0, 0, 0, 0, 6, 0},
+          {0, 0, 0, 0, 0, 0, 0, 9},
+          {0, 0, 0, 0, 0, 0, 0, 0},
+    };
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+           if(graph[i][j] != 0) edges.push_back(Edge(i, j, graph[i][j]));
+    }
+    int cost = 0;
+    vector<Edge> result;
+    parent.resize(n);
+    rank1.resize(n);
+    for (int i = 0; i < n; i++)
+        make_set(i);
+
+    sort(edges.begin(), edges.end());
+
+    for (Edge e : edges) {
+        if (find_set(e.u) != find_set(e.v)) {
+            cost += e.weight;
+            result.push_back(e);
+            union_sets(e.u, e.v);
+        }
+    }
+    for (Edge e : result) {
+        cout << e.u + 1 << " -> " << e.v + 1 << endl;
+    }
+    cout << cost << endl;
+    return 0;
+}
